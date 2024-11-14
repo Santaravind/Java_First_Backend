7f4265304de7d09e2dfe6755d88c054f03b55571
Aravind Sant <aravindsant563@gmail.com>
2024-11-14 13:21:41 +0530

    First Bckend Add ,Update and Delete

---
+++ src/main/java/com/example/Ecomproject/EcomprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ecomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomprojectApplication.class, args);
+	}
+
+}
+
